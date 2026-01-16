797b57e7354ef072b2731c598ad5e31d51d5dc53
gwaihir4 <226409743+gwaihir4@users.noreply.github.com>
2026-01-16 18:56:04 +0300

    problem at script server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,18 @@
+import amqp from "amqplib";
+import process from "node:process";
+
+
 async function main() {
   console.log("Starting Peril server...");
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
+  const connection = "amqp://guest:guest@localhost:5672/";
+  amqp.connect(connection);
+  console.log("Connection OK !")
+  process.on('SIGINT', () => {
+  console.log('Received SIGINT. program is shutting down.');
+});
+  //*console.error("Fatal error:", err);
   process.exit(1);
 });
