b0406e092c095f0bb18ffaea81aab5b7de571d54
gwaihir4 <226409743+gwaihir4@users.noreply.github.com>
2026-01-24 19:45:27 +0300

    client-repl

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,10 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -30,6 +33,54 @@ async function main() {
     PauseKey,
     SimpleQueueType.Transient,
   );
+
+  const gameState = new GameState(username);
+  while(true){
+    const command = await getInput();
+    switch (command[0]) {
+      case "spawn":
+        try {
+          commandSpawn(gameState, command)
+        } catch (err) {
+          console.log((err as Error).message);
+        }
+        break;
+      case "move":
+        try {
+          commandMove(gameState, command);
+        } catch (err) {
+          console.log((err as Error).message);
+        }
+        break;
+      case "status":
+        try {
+          commandStatus(gameState);
+        } catch (err) {
+          console.log((err as Error).message);
+        }
+        break;
+      case "help":
+        try {
+          printClientHelp();
+        } catch (err) {
+          console.log((err as Error).message);
+        }
+        break;
+      case "spam":
+        try {
+          console.log("Spamming not allowed yet!");
+        } catch (err) {
+          console.log((err as Error).message);
+        }
+        break;
+      case "quit":
+        printQuit();
+        process.exit(0);
+      default:
+        console.log(`Command >${command[0]}< not exists`);
+        break;
+    }
+  }
 }
 
 main().catch((err) => {
