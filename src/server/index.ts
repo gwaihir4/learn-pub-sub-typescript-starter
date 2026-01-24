2d4c6e8be1b4664d8d2546ca656653b179ca9283
gwaihir4 <226409743+gwaihir4@users.noreply.github.com>
2026-01-24 19:25:28 +0300

    decoupling

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
-
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
@@ -28,6 +28,32 @@ async function main() {
     });
   } catch (err) {
     console.error("Error publishing message:", err);
+  }
+  printServerHelp();
+  var run = true;
+  while(run){
+    const command = await getInput();
+    switch (command[0]) {
+      case "pause":
+        console.log("Pause command activated. Sending pause message.");
+        publishJSON(publishCh, ExchangePerilDirect, PauseKey, {
+          isPaused: true,
+        });
+        break;
+      case "resume":
+        console.log("Resume command activated. Sending resume message.")
+        publishJSON(publishCh, ExchangePerilDirect, PauseKey, {
+          isPaused: false,
+        });
+        break;
+      case "quit":
+        console.log("Quit command activated exiting system.")
+        run = false;
+        break;
+      default:
+        console.log(`Unrecognized command >${command[0]}<`);
+        break;
+    }
   }
 }
 
